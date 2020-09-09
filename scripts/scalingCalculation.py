f52fccfd64966cfae0e40a9369f83fdb612c4db0
Wendong <wendong.wang11@gmail.com>
2020-09-09 11:16:58 +0200

    scaling calculation file edit

--- scripts/scalingCalculation.py
+++ scripts/scalingCalculation.py
@@ -9,17 +9,67 @@ import scipy.io
 import os
 
 #%% capillary forces
-os.chdir('2020-09-08_capillaryForceCalculations-sym6_radiusScaling')
-result = scipy.io.loadmat('Results_sym6_arcAngle30_eeDistAtHalfR_distStep0pt2um_angleCount60_radiusCount46_corrected')
+if os.getcwd() == 'D:\\simulationFolder\\spinning_rafts_sim2\\':
+    os.chdir('2020-09-08_capillaryForceCalculations-sym6_radiusScaling')
+result = scipy.io.loadmat('Results_sym6_arcAngle30_eeDistAt1R_angleCount60_radiusCount46_treated')
 capForceAngleAveraged = result['angleAveragedForceSqueezed']  # unit: nN
 radiiForCapForce = result['radii']  # unit: micron
+eeDistInR = result['eeDistInR'].item()  # unit: radius
 # convert to SI unit and reshape
-capForceAngleAveragedAtHalfR = capForceAngleAveraged * 1e-9  # unit: N, only at eeDist = radius/2
-radiiForCapForceAtHalfR = radiiForCapForce[0, :] * 1e-6  # unit: m
-# previous wrong capillary calculation
-# surfaceTension = 72e-3  # unit: J/m^2
-# amplitude = 1e-6  # unit: m
-# capForceAngleAvg = np.zeros((len(radiiOfRaft), len(eeDists)))  # unit: N
+capForceAngleAveragedAteeR = capForceAngleAveraged * 1e-9  # unit: N, only at eeDist = radius/2
+radiiForCapForceAteeR = (radiiForCapForce * 1e-6).reshape(capForceAngleAveragedAteeR.shape)  # unit: m
+
+
+#%% magnetic and hydrodynamic lift force calculated at specified eeDist in unit of R
+# eeDistInR = 0.5  # unit: radius
+miu0 = 4 * np.pi * 1e-7  # unit: N/A**2, or T.m/A, or H/m
+magneticMomentPerUnitArea = 1e-8 / (np.pi * (300 * 1e-6) ** 2)  # unit: A (A.m**2/m**2)
+R = radiiForCapForceAteeR  # unit: m. old: np.arange(1, 10001) / 1e6
+densityOfWater = 1e3  # unit: 1000 kg/m^3
+# magDpForceOnAxisAngleAveraged_halfR = np.zeros(len(R))  # unit: N
+# hydroLiftForceAngleAveraged_1rps_halfR = np.zeros(len(R))  # unit: N
+# hydroLiftForceAngleAveraged_10rps_halfR = np.zeros(len(R))  # unit: N
+# hydroLiftForceAngleAveraged_20rps_halfR = np.zeros(len(R))  # unit: N
+
+magDpForceOnAxisAngleAveraged_eeR = -(3 / 4) * miu0 * (np.pi * R ** 2 * magneticMomentPerUnitArea) ** 2 / \
+                                      ((2 + eeDistInR) * R) ** 4
+hydroLiftForceAngleAveraged_1rps_eeR = densityOfWater * (1 * 2 * np.pi) ** 2 * R ** 7 / ((2 + eeDistInR) * R) ** 3
+hydroLiftForceAngleAveraged_10rps_eeR = densityOfWater * (10 * 2 * np.pi) ** 2 * R ** 7 / ((2 + eeDistInR) * R) ** 3
+hydroLiftForceAngleAveraged_20rps_eeR = densityOfWater * (20 * 2 * np.pi) ** 2 * R ** 7 / ((2 + eeDistInR) * R) ** 3
+hydroLiftForceAngleAveraged_70rps_eeR = densityOfWater * (70 * 2 * np.pi) ** 2 * R ** 7 / ((2 + eeDistInR) * R) ** 3
+
+# plotting of forces vs radius range with eeDistance at 0.5R
+fig, ax = plt.subplots(ncols=1, nrows=1)
+ax.loglog(R * 1e3, -magDpForceOnAxisAngleAveraged_eeR, color='blue',
+          label='angle-averaged d-d mag force')
+# ax.loglog(R * 1e3, -magDpForceOnAxisAngleAveraged_halfR/10, color='blue',
+#           label='angle-averaged d-d mag force')
+ax.loglog(R * 1e3, hydroLiftForceAngleAveraged_1rps_eeR, color='orange',
+          label='angle-averaged hydro lift force 1rps')
+ax.loglog(R * 1e3, hydroLiftForceAngleAveraged_10rps_eeR, color='orange',
+          label='angle-averaged hydro lift force 10rps')
+# ax.loglog(R * 1e3, hydroLiftForceAngleAveraged_20rps_halfR, color='orange',
+#           label='angle-averaged hydro lift force 20rps')
+ax.loglog(R * 1e3, hydroLiftForceAngleAveraged_70rps_eeR, color='orange',
+          label='angle-averaged hydro lift force 70rps')
+ax.loglog(radiiForCapForceAteeR * 1e3, capForceAngleAveragedAteeR, color='green',
+          label='angle-averaged capillary force')
+ax.set_xlabel('radius of raft (mm)', size=20)
+ax.set_ylabel('force (N)', size=20)
+ax.set_title('force scaling at eeDist = {}R'.format(eeDistInR))
+ax.legend()
+plt.show()
+figName = 'force scaling at eeDist = {}R.png'.format(eeDistInR)
+fig.savefig(figName)
+
+# save data in a csv file
+radiiCapMagHydroForces = np.concatenate((R, capForceAngleAveragedAteeR, -magDpForceOnAxisAngleAveraged_eeR,
+                                         hydroLiftForceAngleAveraged_1rps_eeR,
+                                         hydroLiftForceAngleAveraged_10rps_eeR,
+                                         hydroLiftForceAngleAveraged_70rps_eeR), axis=1)
+filename = 'force scaling at eeDist = {}R.csv'.format(eeDistInR)
+np.savetxt(filename, radiiCapMagHydroForces, delimiter=',',
+           header='R(m), cap(N), magDp(N), hydro_1rps(N), hydro_10rps(N), hydro_70rps(N)')
 
 #%% magnetic and hydrodynamic lift force calculation based on fixed range of ee-distances
 miu0 = 4 * np.pi * 1e-7  # unit: N/A**2, or T.m/A, or H/m
@@ -38,46 +88,6 @@ for index, R in enumerate(radiiOfRaft):
     hydroLiftForceAngleAveraged_1rps[index, :] = densityOfWater * (1 * 2 * np.pi)**2 * R**7 / (eeDists + 2*R)**3
     hydroLiftForceAngleAveraged_10rps[index, :] = densityOfWater * (10 * 2 * np.pi)**2 * R**7 / (eeDists + 2*R)**3
     hydroLiftForceAngleAveraged_20rps[index, :] = densityOfWater * (20 * 2 * np.pi)**2 * R**7 / (eeDists + 2*R)**3
-
-#%% magnetic and hydrodynamic lift force calculated at eeDist = 0.5*R
-miu0 = 4 * np.pi * 1e-7  # unit: N/A**2, or T.m/A, or H/m
-magneticMomentPerUnitArea = 1e-8 / (np.pi * (300 * 1e-6) ** 2)  # unit: A (A.m**2/m**2)
-R = np.arange(1, 10001) / 1e6  # unit: m
-densityOfWater = 1e3  # unit: 1000 kg/m^3
-# magDpForceOnAxisAngleAveraged_halfR = np.zeros(len(R))  # unit: N
-# hydroLiftForceAngleAveraged_1rps_halfR = np.zeros(len(R))  # unit: N
-# hydroLiftForceAngleAveraged_10rps_halfR = np.zeros(len(R))  # unit: N
-# hydroLiftForceAngleAveraged_20rps_halfR = np.zeros(len(R))  # unit: N
-
-magDpForceOnAxisAngleAveraged_halfR = -(3 / 4) * miu0 * (np.pi * R ** 2 * magneticMomentPerUnitArea) ** 2 / \
-                                      (2.5 * R) ** 4
-hydroLiftForceAngleAveraged_1rps_halfR = densityOfWater * (1 * 2 * np.pi) ** 2 * R ** 7 / (2.5 * R) ** 3
-hydroLiftForceAngleAveraged_10rps_halfR = densityOfWater * (10 * 2 * np.pi) ** 2 * R ** 7 / (2.5 * R) ** 3
-hydroLiftForceAngleAveraged_20rps_halfR = densityOfWater * (20 * 2 * np.pi) ** 2 * R ** 7 / (2.5 * R) ** 3
-hydroLiftForceAngleAveraged_70rps_halfR = densityOfWater * (70 * 2 * np.pi) ** 2 * R ** 7 / (2.5 * R) ** 3
-
-#%% plotting
-# plotting of forces vs radius range with eeDistance at 0.5R
-fig, ax = plt.subplots(ncols=1, nrows=1)
-ax.loglog(R * 1e3, -magDpForceOnAxisAngleAveraged_halfR, color='blue',
-          label='angle-averaged d-d mag force')
-# ax.loglog(R * 1e3, -magDpForceOnAxisAngleAveraged_halfR/10, color='blue',
-#           label='angle-averaged d-d mag force')
-ax.loglog(R * 1e3, hydroLiftForceAngleAveraged_1rps_halfR, color='orange',
-          label='angle-averaged hydro lift force 1rps')
-ax.loglog(R * 1e3, hydroLiftForceAngleAveraged_10rps_halfR, color='orange',
-          label='angle-averaged hydro lift force 10rps')
-# ax.loglog(R * 1e3, hydroLiftForceAngleAveraged_20rps_halfR, color='orange',
-#           label='angle-averaged hydro lift force 20rps')
-ax.loglog(R * 1e3, hydroLiftForceAngleAveraged_70rps_halfR, color='orange',
-          label='angle-averaged hydro lift force 70rps')
-ax.loglog(radiiForCapForceAtHalfR * 1e3, capForceAngleAveragedAtHalfR, color='green',
-          label='angle-averaged capillary force')
-ax.set_xlabel('radius of raft (mm)', size=20)
-ax.set_ylabel('force (N)', size=20)
-ax.set_title('force scaling at eeDist = 0.5R')
-ax.legend()
-plt.show()
 
 
 # plotting at a specific distance value
